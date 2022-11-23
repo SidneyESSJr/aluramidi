9aaf43c4331b204c228a5b7dbf5ccd98dd250ef2
Sidney Junior <sidney.e.s.s.jr@gmail.com>
2022-11-22 22:01:21 -0300

    project done

--- main.js
+++ main.js
@@ -1,14 +1,22 @@
 
 function playSom(idElementeAudio) {
-    document.querySelector(idElementeAudio).play();
+    const elemento = document.querySelector(idElementeAudio);
+    if (elemento && elemento.localName === 'audio') {
+        elemento.play();
+    } else {
+        console.log("Elemento não localizado ou seletor invalido!")
+    }
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let contador = 0;
+listaDeTeclas.forEach((tecla) => {
+    tecla.onclick = () => playSom(`#som_${tecla.classList[1]}`);
+    tecla.onkeydown = (event) => {
+        if (event.key === 'Enter' || event.code === "Space")
+            tecla.classList.add('ativa');
+        tecla.onkeyup = () => tecla.classList.remove('ativa');
+    }
+});
 
-while (contador < listaDeTeclas.length) {
-    listaDeTeclas[contador].onclick = playSom;
-    contador++
-}
 
